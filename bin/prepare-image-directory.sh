4d68c4ff8523eda3b35d71be1ad11b300f754ff9
Matt Gauger <matt.gauger@gmail.com>
2022-07-29 10:21:11 -0500

    images prepare script fixes file extensions too

--- bin/prepare-image-directory.sh
+++ bin/prepare-image-directory.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/bash
 
+# Must be in the directory to prepare
+
+# simplify file extensions
+for i in *.jpeg; do
+  mv -v "$i" "${i%.jpeg}.jpg"
+done
+
+for i in *.JPG; do
+  mv -v "$i" "${i%.JPG}.jpg"
+done
+
+
 for i in *.jpg; do
   printf "$i\n"
   convert $i -format jpg -unsharp 0.25x0.25+8+0.065 -dither None -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip -thumbnail "1920x1080" ${i/.jpg/-resized.jpg}
